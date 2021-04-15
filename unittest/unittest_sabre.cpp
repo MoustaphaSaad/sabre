320511cad06ff6b4ec69bc9131bddad5b685c2ae
MoustaphaSaad <moustapha.saad.abdelhamed@gmail.com>
2021-04-15 23:43:30 +0200

    add declaration parsing unittests

--- unittest/unittest_sabre.cpp
+++ unittest/unittest_sabre.cpp
@@ -112,3 +112,36 @@ TEST_CASE("[sabre]: parse-stmt")
 		}
 	}
 }
+
+TEST_CASE("[sabre]: parse-decl")
+{
+	mn_defer(mn::memory::tmp()->clear_all());
+
+	auto base_dir = mn::path_join(mn::str_tmp(), DATA_DIR, "decl");
+	auto files = mn::path_entries(base_dir, mn::memory::tmp());
+	for (auto f: files)
+	{
+		if (f.kind != mn::Path_Entry::KIND_FILE || f.name == "." || f.name == "..")
+			continue;
+
+		if (mn::str_suffix(f.name, ".out"))
+			continue;
+
+		auto filepath = mn::path_join(mn::str_tmp(), base_dir, f.name);
+		auto out_data = load_out_data(filepath);
+		mn::str_replace(out_data, "\r\n", "\n");
+		mn::str_trim(out_data);
+
+		auto [answer, err] = sabre::parse_decl_from_file(filepath, f.name);
+		CHECK(err == false);
+		mn_defer(mn::str_free(answer));
+
+		auto match = answer == out_data;
+		CHECK(match == true);
+		if (match == false)
+		{
+			mn::print("expected:\n{}\n", out_data);
+			mn::print("answer:\n{}\n", answer);
+		}
+	}
+}
