4a8cc3aac014ddfc5db0191d348cd8a4ec6d8b7f
Mostafa Saad <moustapha.saad.abdelhamed@gmail.com>
2022-03-12 20:32:31 +0200

    add spirv to unit tests

--- unittest/unittest_sabre.cpp
+++ unittest/unittest_sabre.cpp
@@ -421,3 +421,47 @@ TEST_CASE("[sabre]: reflect")
 		}
 	}
 }
+
+TEST_CASE("[sabre]: spirv")
+{
+	mn_defer{mn::memory::tmp()->clear_all();};
+
+	auto base_dir = mn::path_join(mn::str_tmp(), DATA_DIR, "spirv");
+	auto files = mn::path_entries(base_dir, mn::memory::tmp());
+	for (auto f: files)
+	{
+		if (f.kind != mn::Path_Entry::KIND_FILE || f.name == "." || f.name == "..")
+			continue;
+
+		if (mn::str_find_last(f.name, ".out", f.name.count) != SIZE_MAX)
+			continue;
+
+		auto filepath = mn::path_join(mn::str_tmp(), base_dir, f.name);
+
+		if (mn::path_is_file(mn::str_tmpf("{}.out", filepath)) == false)
+		{
+			mn::log_warning("missing spirv output for '{}'", filepath);
+			continue;
+		}
+
+		mn::log_info("testing file: {}...", filepath);
+		auto out_data = load_out_data(filepath);
+		mn::str_replace(out_data, "\r\n", "\n");
+		mn::str_trim(out_data);
+
+		auto [answer_, err] = sabre::spirv_gen_from_file(filepath, f.name, mn::str_lit(""), {});
+		CHECK(err == false);
+		auto answer = answer_;
+		mn_defer{mn::str_free(answer);};
+		mn::str_replace(answer, "\r\n", "\n");
+		mn::str_trim(answer);
+
+		auto match = answer == out_data;
+		CHECK(match == true);
+		if (match == false)
+		{
+			mn::print("expected:\n{}\n", out_data);
+			mn::print("answer:\n{}\n", answer);
+		}
+	}
+}
